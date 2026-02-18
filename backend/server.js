0a33a7da50a85addc5b764dad30ab82a6fcf3a73
Aruzhan152 <aruzhanshamshiyeva@gmail.com>
2026-02-19 02:49:47 +0300

    Реализация заявок

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,13 @@ dotenv.config();
 
 const app = express();
 
+const helmet = require("helmet");
+app.use(helmet());
+
 app.use(cors());
 app.use(express.json());
+
+
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
